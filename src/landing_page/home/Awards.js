6e87efde8420ab57bf01f15fc4dc2cf5abbc7434
user.name like : "Soumen Mandal <user.email like :"soumenmandal1080@gmail.com>
2025-07-02 17:01:10 +0530

    Enhance Awards component layout and content structure

--- src/landing_page/home/Awards.js
+++ src/landing_page/home/Awards.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 function Awards() {
     return (
-        <div className='container'> </div>
+        <div className='container mt-5 mb-5'>
+         <div className='row'>
+          <div className='col-6'><img src = "media\largestBroker.svg" style={{width: "100%"}}/></div>
+          <div className='col-6'>
+            <h1>Largest stock broker in india</h1>
+            <p>2+million Zerodha clients contribute to over 15% of all retil order volumes in india daily by trading and investing in .</p>
+            <div className='row'>
+            <div className='col-6'>
+            <ul>
+              <li>Future and options </li>
+              <li>Commodity derivatives</li>
+              <li>current derivatives</li>
+            </ul>
+            
+          </div>
+          <div className='col-6'><ul>
+              <li>Stock and IPOs </li>
+              <li>Direct meutal funds</li>
+              <li>Bonds and Govt. securites</li>
+            </ul></div>
+          </div>
+         <img src="media\pressLogos.png" style={{width: "100%"}} />
+          </div> 
+          
+           </div>
+           </div>
+
       );
 }
 
 export default Awards;
+
+
+          
